412d5d86c1cfb8fbfe7c2cecfa90ecd304ee1790
Khoa230700 <yujidream2307@gmail.com>
2025-02-21 14:54:29 +0700

    123

---
+++ Assets/Script/Spawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private Transform[] spawnPoints = new Transform[3];
+    private int Enemy = 0;
+    private void Start()
+    {
+        
+        // Khởi chạy coroutine để spawn quái
+        StartCoroutine(SpawnEnemies());
+    }
+    IEnumerator SpawnEnemies()
+    {
+       
+        while (Enemy <= 60)
+        {
+            for (int i = 0; i < spawnPoints.Length; i++)
+            {
+                Instantiate(enemyPrefab, spawnPoints[i].position , spawnPoints[i].rotation);
+                Enemy++;
+                Debug.Log(Enemy);
+                yield return new WaitForSeconds(2);
+            }
+            yield return new WaitForSeconds(5);
+        }
+    }
+
+}
+
