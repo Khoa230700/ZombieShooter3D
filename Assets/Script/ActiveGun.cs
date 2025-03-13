7b1c0599c6de81e5808a02d46329c6007f397d7f
Andy THR <trunghauphan2710@gmail.com>
2025-03-13 19:32:18 +0700

    firebase update

--- Assets/Script/ActiveGun.cs
+++ Assets/Script/ActiveGun.cs
@@ -13,7 +13,7 @@ public class ActiveGun : MonoBehaviour
     float timelastshot;
     void Start()
     {
-        currentshooter = FindObjectOfType<RaycastShooter>();
+        //currentshooter = FindObjectOfType<RaycastShooter>();
     }
 
     // Update is called once per frame
@@ -32,7 +32,7 @@ public class ActiveGun : MonoBehaviour
         {
             if (timelastshot >= gunso.FireRate)
             {
-                currentshooter.CheckMouseClick(gunso);
+                //currentshooter.CheckMouseClick(gunso);
                 //flash.Play();
                 adusource.PlayOneShot(aduclip);
                 animator.Play("RilfeShoot", 0, 0);
