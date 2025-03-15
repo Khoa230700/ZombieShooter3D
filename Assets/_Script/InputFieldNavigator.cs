4604a25d6a82ab1c9782caa5f303b5a787c8e89a
Andy THR <trunghauphan2710@gmail.com>
2025-03-16 00:44:05 +0700

    update

---
+++ Assets/_Script/InputFieldNavigator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InputFieldNavigator : MonoBehaviour
+{
+    public InputField[] inputFields;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            MoveToNextInputField();
+        }
+    }
+
+    private void MoveToNextInputField()
+    {
+        int currentIndex = -1;
+
+        for (int i = 0; i < inputFields.Length; i++)
+        {
+            if (EventSystem.current.currentSelectedGameObject == inputFields[i].gameObject)
+            {
+                currentIndex = i;
+                break;
+            }
+        }
+
+        if (currentIndex != -1)
+        {
+            int nextIndex = (currentIndex + 1) % inputFields.Length; // Quay lại đầu khi đến ô cuối
+            SetFocusOnInputField(nextIndex);
+        }
+    }
+
+    private void SetFocusOnInputField(int index)
+    {
+        inputFields[index].Select();
+        inputFields[index].ActivateInputField();
+    }
+}
